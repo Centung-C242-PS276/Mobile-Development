80fcdf59d15c98a7d788995f93c6af98e9af6f5b
Muhammadzakirin <zakirinputra191@gmail.com>
2024-11-30 23:42:36 +0700

    Refactor SplashActivity: Implement View Binding and Add Splash Delay
    - Added a coroutine to introduce a splash screen delay before navigating to WelcomeActivity.

--- app/src/main/java/com/capstone/centung/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/capstone/centung/ui/splash/SplashActivity.kt
@@ -1,21 +1,32 @@
 package com.capstone.centung.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.capstone.centung.R
+import androidx.lifecycle.lifecycleScope
+import com.capstone.centung.databinding.ActivitySplashBinding
+import com.capstone.centung.ui.welcome.WelcomeActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycleScope.launch {
+            delay(SPLASH_DURATION.seconds)
+            startActivity(Intent(this@SplashActivity, WelcomeActivity::class.java))
+            finish()
         }
+
+    }
+
+    companion object {
+        const val SPLASH_DURATION = 3
     }
 }
