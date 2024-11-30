962011e7d6bf5a21967fe94d635c60b3cbc88805
Muhammadzakirin <zakirinputra191@gmail.com>
2024-11-30 23:40:39 +0700

    Refactor LoginActivity: Implement View Binding and Add Navigation
    - Integrated navigation to RegisterActivity and MainActivity upon button clicks.

--- app/src/main/java/com/capstone/centung/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/centung/ui/login/LoginActivity.kt
@@ -1,21 +1,35 @@
 package com.capstone.centung.ui.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.capstone.centung.R
+import com.capstone.centung.databinding.ActivityLoginBinding
+import com.capstone.centung.ui.bottom_navbar.MainActivity
+import com.capstone.centung.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupButtonActions()
+
+    }
+
+    private fun setupButtonActions() {
+        binding.apply {
+            tvBtnRegister.setOnClickListener {
+                startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+            }
+
+            btnLogin.setOnClickListener {
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                finish()
+            }
         }
     }
+
+
 }
