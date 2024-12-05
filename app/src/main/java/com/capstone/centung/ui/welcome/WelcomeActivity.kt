d7e9b2b3b9ec81fe4b355ba28441d64156c7a19a
Muhammadzakirin <zakirinputra191@gmail.com>
2024-12-05 23:15:20 +0700

    Removed button action on WelcomeActivity moved into FirstActivity

--- app/src/main/java/com/capstone/centung/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/capstone/centung/ui/welcome/WelcomeActivity.kt
@@ -1,11 +1,8 @@
 package com.capstone.centung.ui.welcome
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.centung.databinding.ActivityWelcomeBinding
-import com.capstone.centung.ui.login.LoginActivity
-import com.capstone.centung.ui.register.RegisterActivity
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
@@ -13,23 +10,9 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupButtonActions()
+
 
     }
 
-    private fun setupButtonActions() {
-        binding.apply {
-            btnLogin.setOnClickListener {
-                setupIntent(LoginActivity::class.java)
-            }
-            btnSignUp.setOnClickListener {
-                setupIntent(RegisterActivity::class.java)
-            }
-        }
-    }
 
-    private fun setupIntent(destination: Class<*>) {
-        startActivity(Intent(this@WelcomeActivity, destination))
-        finish()
-    }
 }
