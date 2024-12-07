4c2eea6dac3da9a4f4a98be10d2b66e13ea41835
Muhammadzakirin <zakirinputra191@gmail.com>
2024-12-07 10:48:19 +0700

    Add actions to profile card

--- app/src/main/java/com/capstone/centung/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstone/centung/ui/profile/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.capstone.centung.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.capstone.centung.databinding.FragmentProfileBinding
+import com.capstone.centung.ui.about_app.AboutAppActivity
+import com.capstone.centung.ui.login.LoginActivity
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -22,9 +25,29 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupButtonActions()
 
     }
 
+    private fun setupButtonActions() {
+        binding.apply {
 
+            btnAboutApp.setOnClickListener {
+                startActivity(Intent(requireActivity(), AboutAppActivity::class.java))
+            }
+
+            btnRatingUs.setOnClickListener {
+
+            }
+
+            btnContactUs.setOnClickListener {
+
+            }
+
+            btnLogout.setOnClickListener {
+                startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                requireActivity().finish()
+            }
+        }
+    }
 }
--- app/src/main/java/com/capstone/centung/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/capstone/centung/ui/welcome/WelcomeActivity.kt
@@ -1,8 +1,10 @@
 package com.capstone.centung.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.centung.databinding.ActivityWelcomeBinding
+import com.capstone.centung.ui.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
@@ -10,8 +12,9 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        binding.btnGetStarted.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
 
