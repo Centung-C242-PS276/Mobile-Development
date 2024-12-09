6e039c6353f3f48d4709769199598d308c69e858
rafatazakka <muhammadrafagithub@gmail.com>
2024-12-09 15:03:09 +0700

    menghubungkan navigasi

--- app/src/main/java/com/capstone/centung/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/capstone/centung/ui/dashboard/DashboardFragment.kt
@@ -4,39 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.capstone.centung.R
 import com.capstone.centung.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
-    private var _binding: FragmentDashboardBinding? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+        // Komponen layout
+        val statusText: TextView = view.findViewById(R.id.currentStatus)
+        val growthChart: ImageView = view.findViewById(R.id.growthChart)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Set status anak
+        val predictionResult = arguments?.getString("predictionResult") ?: "NORMAL"
+        statusText.text = predictionResult
+        if (predictionResult == "STUNTING") {
+            statusText.setBackgroundResource(R.drawable.status_indicator_warning)
+        } else {
+            statusText.setBackgroundResource(R.drawable.status_indicator_safe)
         }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
